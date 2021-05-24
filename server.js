acae44a8250a63ad0af4c11824626351056c3f4f
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2021-05-24 14:53:28 +0530

    first commit

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.post("/api/token/:userId", (req, res) => {
 	});
 });
 
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server is running at PORT ${PORT}`));
